d34f13bd918ba1fa4bbafc22c90ce2536c4114ea
Eunryong <srdn45@gmail.com>
2025-07-06 19:21:25 +0900

    fix: import file name error

--- backend/src/stock/StockModule.ts
+++ backend/src/stock/StockModule.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DartModule } from './dart/dart.module';
+import { DartModule } from './dart/DartModule';
 
 @Module({
   imports: [
