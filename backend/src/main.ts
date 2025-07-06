cea35a49cfd12569e6e182e3ad365b68fc4dee5d
jihkang <mallangcoogi@gmail.com>
2025-07-06 20:52:23 +0900

    Feat: SwaggerSetting initalize (#33)
    Feat: rebase and remove unnecessary files (#33)
    
    Feat: swagger package.json add, fix lint
    
    Feat: Fix lint and package.json update (#33)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  // set swagger in development mode
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Invest friends API')
+      .setDescription('The invest friends API description')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1', app, document);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
